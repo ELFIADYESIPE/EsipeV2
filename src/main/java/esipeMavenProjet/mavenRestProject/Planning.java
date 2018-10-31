db23f113d465e6c036ff2dd5920a0194009f8d32
LudovicFoltete <ludovicflb@gmail.com>
2018-10-31 16:11:46 +0100

    modifier planning implémenté

---
+++ src/main/java/esipeMavenProjet/mavenRestProject/Planning.java
@@ -0,0 +1,12 @@
+package esipeMavenProjet.mavenRestProject;
+
+public class Planning {
+
+	public Planning() {
+		
+	}
+	
+	public void modiferPlanning() {
+		System.out.println("Je modifie le planning");
+	}
+}
